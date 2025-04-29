626537b42d6662ad9fcc96892a0388a3499cedc0
Lucas Gomes Santana <ls7128387@gmail.com>
2025-04-29 15:19:42 -0300

    Inicializando o projeto

---
+++ back-end/src/test/java/com/br/back_end/BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.back_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
